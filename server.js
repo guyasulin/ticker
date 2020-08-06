69a22fb94064596187fe7e708991d1c2db7bc992
guy <guyasulin13@gmail.com>
2020-08-06 16:14:07 +0300

    .

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(__dirname + '/dist/ticker'));
+app.get('/*', function(req, res){
+    res.sendFile(path.join(__dirname + '/dist/ticker/index.html'))
+})
+app.listen(process.env.PORT || 8080);
