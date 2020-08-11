4b57074549e13b15227d618984ba8e56e093eaad
guy <guyasulin13@gmail.com>
2020-08-11 11:20:06 +0300

    deplying to heroku

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
 
 app.use(express.static(__dirname + '/dist/ticker'));
 app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname + '/dist/ticker/index.html'))
-})
+    res.sendFile(path.join(__dirname + '/dist/ticker/index.html'));
+});
 app.listen(process.env.PORT || 8080);
